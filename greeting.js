78cbc4220815ab1188b3d79d2073ffdf3f7e7e3b
Jaewon Han <jaewonhan20@gmail.com>
2021-03-21 19:50:09 -0500

    saved user's input name and display

--- greeting.js
+++ greeting.js
@@ -2,20 +2,36 @@ const form=document.querySelector(".js-form"),
     input = form.querySelector("input");//gets any selector in a css way. class/ tag/ id...
     greeting=document.querySelector(".js-greetings");
 
-const USER_LS="currentUser",
+const USER_LS="currentUser",  //LS=local storage
     SHOWING_CN="showing";
 
+ 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+    }
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue=input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text){
-    form.classList.remove(Showing_CN);
-    greeting.classList.add(Showing_CN);
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
     greeting.innerText=`Hello ${text}`;
 }
 
 function loadName(){
-    const currentUswer=localStorage.getItem(USER_LS);
+    const currentUser=localStorage.getItem(USER_LS);
     if(currentUser===null){
-
+        askForName();
     }
     else{
         paintGreeting(currentUser);
